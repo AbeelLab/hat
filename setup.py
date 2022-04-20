1e18fd409ccd8623f50581214b95be41ab2ac64c
Ramin Shirali <r.shirali.hz@gmail.com>
2022-04-20 15:22:13 +0200

    added versions to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='HAT_phasing',
-    version='0.0.2',
+    version='0.0.3',
     packages=['hat'],
     url='',
     license='GPL-3.0-or-later',
@@ -13,7 +13,11 @@ setup(
     author_email='r.shirali.hz@gmail.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=['pysam', 'numpy', 'seaborn', 'matplotlib', 'biopython'],
+    install_requires=['pysam==1.19.0',
+                      'numpy>=1.22.3',
+                      'seaborn',
+                      'matplotlib',
+                      'biopython==1.79'],
     entry_points={'console_scripts': ['HAT = hat.HAT_main:main']},
     python_requires=">=3.6",
     description='HAT:‌  Haplotype assembly tool that use both long and short reads to reconstruct haplotypes'
